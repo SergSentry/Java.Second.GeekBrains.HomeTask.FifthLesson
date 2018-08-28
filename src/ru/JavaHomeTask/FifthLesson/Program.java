9f352d7f3bbb1494fca21e8d20495d1c560feafd
Serg <rtfm.faq@mail.ru>
2018-08-28 12:47:35 +0300

    FifthHomeWork: Создан пустой проект в IntelliJ IDEA, и прописан метод main()

---
+++ src/ru/JavaHomeTask/FifthLesson/Program.java
@@ -0,0 +1,16 @@
+package ru.JavaHomeTask.FifthLesson;
+
+/**
+ * Класс программы домашнего задания пятого урока
+ */
+public class Program {
+
+    /**
+     * Точка входа в приложение
+     *
+     * @param args параметры командной строки
+     */
+    public static void main(String[] args) {
+
+    }
+}
